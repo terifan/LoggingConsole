d177df2b64033edf821f3b5ff9fef885716ee150
terifan <pinjata69@gmail.com>
2016-04-20 13:43:36 +0200

    updated

--- src/org/terifan/logging_console/Application.java
+++ src/org/terifan/logging_console/Application.java
@@ -1,18 +1,22 @@
 package org.terifan.logging_console;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 import javax.swing.JFrame;
 import org.terifan.util.log.Log;
 
 
 public class Application
 {
-	private LogView mLogView;
+	private static LogView mLogView;
+
 	private JFrame mFrame;
 
 
 	public Application()
 	{
-		mLogView = new LogView(new LogModel());
+		mLogView = new LogView();
 
 		mFrame = new JFrame();
 		mFrame.add(mLogView);
@@ -53,11 +57,30 @@ public class Application
 
 			new Thread()
 			{
+				{
+					setDaemon(true);
+				}
 				@Override
 				public void run()
 				{
 					try
 					{
+						Random rnd = new Random();
+						String[] tags = {"org.terifan.efs.core.BlockDevice","org.terifan.efs.core.FileStream","org.terifan.efs.hashtable.HashTable"};
+						String[] logTypes = {"DEBUG", "VERBOSE", "INFO", "WARN", "ERROR", "SEVERE"};
+						int[] colors = {0xD00000,0x00D000,0x0000D0,0xD000D0,0xD0D000,0x00D0D0};
+						SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss.SSS");
+
+						for (int i = 0, depth = 0; ; i++)
+						{
+							String tag = tags[rnd.nextInt(tags.length)];
+
+							mLogView.add(new LogRow(timeFormat.format(new Date()), logTypes[i%6], depth, colors[i%6], tag, RandomSentenceGenerator.next()));
+
+							depth = Math.min(Math.max(depth + rnd.nextInt(3) - 1, 0), 5);
+
+							Thread.sleep(i < 100 ? 0 : 1000);
+						}
 					}
 					catch (Throwable e)
 					{
--- src/org/terifan/logging_console/LogView.java
+++ src/org/terifan/logging_console/LogView.java
@@ -1,39 +1,129 @@
 package org.terifan.logging_console;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
-import java.awt.event.AdjustmentEvent;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.event.AdjustmentListener;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
+import org.terifan.ui.Utilities;
 
 
 public class LogView extends JPanel
 {
-	private LogModel mModel;
-	private LogPanel mLogPanel;
+	private Color mSeparatorColor = new Color(245,245,245);
+	private Color mStrokeColor = new Color(128,128,128);
+	private Stroke mLineStroke = new BasicStroke(1, 0, 0, 1, new float[]{1f}, 0f);
+
+	private int mCounterWidth = 55;
+	private int mTimeWidth = 90;
+	private int mTypeWidth = 100;
+	private int mTagWidth = 250;
+	private int mIndentWidth = 50;
+	private int mRowHeight = 17;
+
+	private TreeMap<Integer,LogRow> mRows;
+	private AtomicInteger mCounter;
+	private JScrollBar mScrollBar;
 
 
-	public LogView(LogModel aModel)
+	public LogView()
 	{
 		super(new BorderLayout());
 
-		mModel = aModel;
+		mCounter = new AtomicInteger();
+		mRows = new TreeMap<>();
 
-		JScrollBar sb = new JScrollBar(JScrollBar.VERTICAL, 0, 41, 0, mModel.size());
+		mScrollBar = new JScrollBar(JScrollBar.VERTICAL, 0, 41, 0, Math.max(41, mRows.size()));
+		mScrollBar.addAdjustmentListener(mAdjustmentListener);
 
-		mLogPanel = new LogPanel(this, sb);
-
-		sb.addAdjustmentListener(mAdjustmentListener);
+		mLogPanel.setBackground(Color.WHITE);
+		mLogPanel.setOpaque(true);
 
 		super.add(mLogPanel, BorderLayout.CENTER);
-		super.add(sb, BorderLayout.EAST);
+		super.add(mScrollBar, BorderLayout.EAST);
 	}
 
 
-	public LogModel getModel()
+	public void add(LogRow aLogRow)
 	{
-		return mModel;
+		if (mRows.size() > 100)
+		{
+			mRows.remove(mRows.firstKey());
+		}
+
+		mRows.put(mCounter.getAndIncrement(), aLogRow);
+
+		mScrollBar.setMaximum(Math.max(41, mRows.size()));
+		mScrollBar.invalidate();
+		mLogPanel.invalidate();
+		repaint();
 	}
+
+
+	private JPanel mLogPanel = new JPanel()
+	{
+		@Override
+		protected void paintComponent(Graphics aGraphics)
+		{
+			Graphics2D g = (Graphics2D)aGraphics;
+
+			Utilities.enableTextAntialiasing(g);
+
+			super.paintComponent(g);
+
+			int w = getWidth();
+			int h = getHeight();
+			int y = 0;
+
+			Stroke oldStroke = g.getStroke();
+
+			int f = mRows.isEmpty() ? 0 : mRows.firstKey();
+
+			for (int rowIndex = mScrollBar.getValue(); y < h && rowIndex < mRows.size(); rowIndex++)
+			{
+				y += mRowHeight;
+
+				LogRow row = mRows.get(f + rowIndex);
+
+				g.setColor(new Color(row.getColor()));
+				g.drawString(Integer.toString(f + rowIndex), 0, y - 4);
+				g.drawString(row.getTime(), mCounterWidth, y - 4);
+				g.drawString(row.getLogType(), mCounterWidth + mTimeWidth, y - 4);
+				g.drawString(row.getTag(), mCounterWidth + mTimeWidth + mTypeWidth, y - 4);
+				g.drawString(row.getMessage(), mCounterWidth + mTimeWidth + mTypeWidth + mTagWidth + mIndentWidth * row.getDepth(), y - 4);
+
+				g.setColor(mStrokeColor);
+				g.setStroke(mLineStroke);
+				for (int i = 0; i < row.getDepth(); i++)
+				{
+					int x = mCounterWidth + mTimeWidth + mTypeWidth + mTagWidth + mIndentWidth * i;
+					g.drawLine(x, y - mRowHeight + 2, x, y - 1);
+				}
+				g.setStroke(oldStroke);
+
+				g.setColor(mSeparatorColor);
+				g.drawLine(0, y, w, y);
+			}
+		}
+
+
+		@Override
+		public Dimension getPreferredSize()
+		{
+			Dimension dim = super.getPreferredSize();
+			dim.height = mRows.size() * mRowHeight;
+			return dim;
+		}
+	};
 
 
 	private transient AdjustmentListener mAdjustmentListener = (e) ->
